282720047983d1035f5b35a88a73e28fb0f0632a
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-12-28 03:21:49 +0000

    thank you page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,9 @@
+export default function ThankYou() {
+    return (
+        <main>
+            <h1 className="text-xl grid place-content-center min-h-screen">
+                Thank you for your feedback!
+            </h1>
+        </main>
+    )
+}
